85057fbebfc283da759b6c50d5860c55b1c3ec35
shaktilucifer <shakticse@gmail.com>
2026-02-15 12:54:49 -0500

    Add delete name, print consolidation

--- assassin game/AssassinManager.java
+++ assassin game/AssassinManager.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 class AssassinManager implements GameManager {
     AssassinNode head;
+    AssassinNode graveYardHead;
     AssassinManager(List<String> gameList) {
         AssassinNode temp = null;
         for(int i = 0; i < gameList.size(); i++) {
@@ -27,11 +28,26 @@ class AssassinManager implements GameManager {
 
     @Override
     public void printGraveyard() {
+        printFromHead(graveYardHead);
+    }
 
+    private void printFromHead(AssassinNode startingNode) {
+        AssassinNode temp = startingNode;
+        while(temp != null) {
+            System.out.println(temp.name);
+            temp = temp.next;
+        }
     }
 
     @Override
     public boolean killRingContains(String name) {
+        AssassinNode temp = head;
+        while(temp != null) {
+            if(name.equals(temp.name)) {
+                return true;
+            }
+            temp = temp.next;
+        }
         return false;
     }
 
@@ -52,6 +68,25 @@ class AssassinManager implements GameManager {
 
     @Override
     public void kill(String name) {
+        AssassinNode curr = head;
+        AssassinNode temp;
+        if(curr.name.equals(name)) {
+            temp = curr;
+            head = curr.next;
+            temp.next = graveYardHead;
+            graveYardHead = temp;
+            return;
+        }
 
+        while (curr.next != null) {
+            if(curr.next.name.equals(name)) {
+                temp = curr.next;
+                curr.next = curr.next.next;
+                temp.next = graveYardHead;
+                graveYardHead = temp;
+                return;
+            }
+            curr = curr.next;
+        }
     }
 }
